00e0eebbe98d54455d99447c48243e04a290cbc7
Sta-ces <staescedric@gmail.com>
2018-02-02 23:09:21 +0100

    Character move

---
+++ Assets/_Project/Scritps/CharacterControlerGames.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+[RequireComponent(typeof(Rigidbody))]
+public class CharacterControlerGames : MonoBehaviour {
+
+    public int m_IDPlayerController = 0;
+    [Range(1f, 10f)]
+    public float m_SpeedCharacter = 5f;
+
+
+    private void Awake()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        m_rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+        m_player = ReInput.players.GetPlayer(m_IDPlayerController);
+    }
+
+    private void Update()
+    {
+        Vector3 velocity = m_rigidbody.velocity;
+
+        velocity.z = m_player.GetAxis("MoveZ") * m_SpeedCharacter;
+        velocity.x = m_player.GetAxis("MoveX") * m_SpeedCharacter;
+
+        m_rigidbody.velocity = velocity;
+    }
+
+
+    private Rigidbody m_rigidbody;
+    private Player m_player;
+}
