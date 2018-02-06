91ddad2f7951f514755c24d0cc133f075bc90d71
Sta-ces <staescedric@gmail.com>
2018-02-06 18:22:13 +0100

    Pause Menu

---
+++ Assets/_Project/Scritps/PauseMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using Rewired;
+
+public class PauseMenu : MonoBehaviour {
+
+    public Canvas m_MenuInGame;
+
+
+    private void Update()
+    {
+        Player input = CharacterControlerGames.PlayerInput;
+        if ( input.GetButtonDown("Pause") )
+            Paused();
+        if ( input.GetButtonDown("Cancel") && m_MenuInGame.gameObject.activeSelf )
+            Paused();
+    }
+
+    private void Paused()
+    {
+        m_MenuInGame.gameObject.SetActive(!m_MenuInGame.gameObject.activeSelf);
+        LoadingLevels.PausedGame();
+    }
+}
