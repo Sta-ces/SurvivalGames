c3181f44ec8333288b87511df6a77a8af6e52593
Staces <staescedric@gmail.com>
2018-05-11 23:02:52 +0200

    SunRotation script

---
+++ Assets/_Project/Scripts/SunScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class SunScript : MonoBehaviour {
+
+	[Range(0.1f,10f)]
+	public float SpeedSunRotation = 1f;
+
+	void Update(){
+		transform.Rotate(0, SpeedSunRotation * Time.deltaTime, 0);
+	}
+}
