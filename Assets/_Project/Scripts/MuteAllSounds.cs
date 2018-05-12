5da6b33c6d91e8f2ebe0eda8320b1a616825c3c6
Staces <staescedric@gmail.com>
2018-05-12 18:27:35 +0200

    Mute All Sounds

--- Assets/_Project/Scripts/MuteAllSounds.cs
+++ Assets/_Project/Scripts/MuteAllSounds.cs
@@ -2,8 +2,6 @@
 
 public class MuteAllSounds : SimpleSingleton<MuteAllSounds> {
 
-	public AudioListener ListenerToMute;
-
 	private bool mutes = false;
 	public bool Mutes{
 		get{ return mutes; }
@@ -11,17 +9,7 @@ public class MuteAllSounds : SimpleSingleton<MuteAllSounds> {
 	}
 
 	public void MuteSounds(){
-		AudioListener audio;
-
-		if(ListenerToMute == null){
-			if(GetComponent<AudioListener>())
-				audio = GetComponent<AudioListener>();
-			else audio = null;
-		} else audio = ListenerToMute;
-
-		if(audio != null){
-			audio.enabled = !audio.enabled;
-			MuteAllSounds.Instance.Mutes = audio.enabled;
-		} else Debug.LogError("Audio Listener Miss");
+		AudioListener.volume = AudioListener.volume == 0 ? 1 : 0;
+		MuteAllSounds.Instance.Mutes = AudioListener.volume == 0 ? true : false;
 	}
 }
