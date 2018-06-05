38d4a4d7b6a5cc7e1d529c94fbfd682c9b4cff9b
Staces <staescedric@gmail.com>
2018-06-05 10:44:12 +0200

    Affiche Texte Pouvoir

--- Assets/SkillTrees/Scripts/SkillUsedText.cs
+++ Assets/SkillTrees/Scripts/SkillUsedText.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Text))]
 public class SkillUsedText : MonoBehaviour {
-
+    
     public float TimeBeforeDisappear = 1;
 
 	public void SkillUsed(SkillsButtons skill)
     {
+        StartCoroutine(Waiting(skill.skill.Name));
+    }
 
+    private IEnumerator Waiting(string _name)
+    {
+        GetComponent<Text>().text = _name;
+        yield return new WaitForSeconds(TimeBeforeDisappear);
+        GetComponent<Text>().text = "";
     }
 }
