f176530766347eec123c43fcbaae6c8f92a50ba8
Staces <staescedric@gmail.com>
2018-06-16 23:42:32 +0200

    End Game Credits

---
+++ Assets/_Project/Scripts/PlayerPrefParam.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class PlayerPrefParam : MonoBehaviour {
+
+	public void PlayerPrefSuperTiki(bool _superTiki)
+    {
+        PlayerPrefs.SetString("SuperTikiPower", _superTiki.ToString().ToLower());
+    }
+}
---
+++ Assets/_Project/Videos/Scripts/Videos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Video;
+
+[RequireComponent(typeof(VideoPlayer))]
+public class Videos : MonoBehaviour {
+
+    [Header("At The End of the Video")]
+    public UnityEvent EndVideo;
+	
+	void Update ()
+    {
+        long frame = GetComponent<VideoPlayer>().frame;
+        long frameCount = Convert.ToInt64(GetComponent<VideoPlayer>().frameCount);
+
+        if (frame == frameCount)
+            EndVideo.Invoke();
+	}
+}
