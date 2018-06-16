a511c8038f4a5ee96e9119e78da106d5565b2943
Staces <staescedric@gmail.com>
2018-06-16 11:33:49 +0200

    Video

---
+++ Assets/_Project/Scripts/StraightAhead.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class StraightAhead : MonoBehaviour {
+
+    [Range(0, 20)]
+    public float Speed = 1;
+	
+	void Update () {
+        transform.localPosition -= (Vector3.forward * Speed);
+	}
+}
