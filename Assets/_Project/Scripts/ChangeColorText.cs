f6bbac47049932ebfcd59b26e4962a4140fb6b11
Staces <staescedric@gmail.com>
2018-06-19 15:57:36 +0200

    Affichage Hail to Gods

---
+++ Assets/_Project/Scripts/ChangeColorText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class ChangeColorText : MonoBehaviour {
+
+    public Color NewColor;
+
+	public void ChangeColor()
+    {
+        GetComponent<Text>().color = NewColor;
+    }
+
+    public void ResetColor()
+    {
+        GetComponent<Text>().color = baseColor;
+    }
+
+
+    private void Awake()
+    {
+        baseColor = GetComponent<Text>().color;
+    }
+
+    private Color baseColor;
+}
