c77fcc14b3262199cd52bb9c1daeb576ad193b9f
Staces <staescedric@gmail.com>
2018-06-19 13:26:32 +0200

    Reverting End Game Credits

--- Assets/_Project/Scripts/PlayerPrefParam.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class PlayerPrefParam : MonoBehaviour {
-
-	public void PlayerPrefSuperTiki(bool _superTiki)
-    {
-        PlayerPrefs.SetString("SuperTikiPower", _superTiki.ToString().ToLower());
-    }
-}
