4f329745096be3d5964f6f28bf4aa6b7bf55599e
Staces <staescedric@gmail.com>
2018-06-19 15:36:47 +0200

    Super Tiki Power

--- Assets/SkillTrees/Scripts/SkillEffects/Endofthegame.cs
+++ Assets/SkillTrees/Scripts/SkillEffects/Endofthegame.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class Endofthegame : SimpleSingleton<Endofthegame> {
-
-    public static bool EndGame = false;
+    
+    public static bool EndGame
+    {
+        get { return PlayerPrefs.GetString("SuperTikiPower", "false").ToLower() == "true"; }
+        set { PlayerPrefs.SetString("SuperTikiPower", value.ToString().ToLower()); }
+    }
     public void SetEndGame(bool _end)
     { EndGame = _end; }
 
@@ -13,15 +17,15 @@ public class Endofthegame : SimpleSingleton<Endofthegame> {
     [Header("Activate")]
     public UnityEvent OnActivate;
 
-    [Header("Active After")]
-    public int NumberKill = 50;
-
     [Header("Finish the Game")]
     public UnityEvent OnFinish;
 
 
     [Header("Super Tiki")]
     public UnityEvent OnSuperPower;
+    public int NumberKill = 50;
+    [Range(0, 20)]
+    public float SpeedShoot = 1f;
 
     private static int countingKill = 0;
     public static int CountingKill
@@ -45,7 +49,7 @@ public class Endofthegame : SimpleSingleton<Endofthegame> {
 
     public void FinishIt()
     {
-        if (!EndGame && Spawning.Instance.IsFinish && GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
+        if (Spawning.Instance.IsFinish && GameObject.FindGameObjectsWithTag("Enemy").Length <= 0)
         {
             print("FINISH!!!");
             OnFinish.Invoke();
@@ -70,29 +74,43 @@ public class Endofthegame : SimpleSingleton<Endofthegame> {
 
     private IEnumerator SuperTikiPower()
     {
+        GameManager.OnPlay = false;
+        Weapons.Instance.IsShoot = false;
+        GameManager.IsPaused = true;
+
         GameObject Charact = CharacterControler.Instance.gameObject;
         GameObject[] Enemies = GameObject.FindGameObjectsWithTag("Enemy");
         float enemySpeed = EnemyCapacity.SpeedEnemy;
-
-        while (Enemies.Length > 0)
+        EnemyCapacity.SpeedEnemy = 0.25f;
+        
+        foreach (GameObject enemy in Enemies)
         {
-            EnemyCapacity.SpeedEnemy = 0.25f;
-            foreach(GameObject enemy in Enemies)
+            if(enemy != null)
             {
                 Charact.transform.LookAt(enemy.transform);
                 Weapons.Instance.AutomaticShooting();
-                yield return new WaitForSeconds(AutomaticShoot.Instance.CoolDownShoot);
+                yield return new WaitForSeconds(SpeedShoot);
             }
         }
+
         EnemyCapacity.SpeedEnemy = enemySpeed;
+        GameManager.IsPaused = false;
+        Weapons.Instance.IsShoot = true;
+        GameManager.OnPlay = true;
     }
 
     private void Update()
     {
-        if (PlayerPrefs.GetString("SuperTikiPower","false").ToLower() == "true" && Controls.SuperTiki)
+        if(PlayerPrefs.GetString("SuperTikiPower", "false").ToLower() == "true")
         {
-            print("Super Tiki!");
-            OnSuperPower.Invoke();
+            if(CountingKill >= NumberKill)
+            {
+                if (Controls.SuperTiki)
+                {
+                    print("Super Tiki!");
+                    OnSuperPower.Invoke();
+                }
+            }
         }
         
         if(FindObjectsOfType<Spawning>().Length > 0)
